268e318b3c784812c05cfa9af070266dca278a42
Italo Carlo <italocarlo43@gmail.com>
2023-09-27 18:45:55 -0300

    inclusao da class Hello Word

---
+++ progeto.video.git/src/main/java/progeto/video/git/HelloWord.java
@@ -0,0 +1,10 @@
+package progeto.video.git;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Word");
+	}
+
+}
